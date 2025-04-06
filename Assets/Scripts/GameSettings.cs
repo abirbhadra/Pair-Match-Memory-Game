3b5c2e07c9f7a3c9fe484348057b18e693563bdf
abirbhadra <58373108+abirbhadra@users.noreply.github.com>
2025-04-07 02:46:11 +0530

    SetGame Scene all buttons funcationality implemented
    SetGame Scene all buttons including 10 , 15 , 20 pairs and vegs , fruits categories and backbutton functionalities are implemented.

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    private readonly Dictionary<EPuzzleCategories, string> _puzzleGameDirectory = new Dictionary<EPuzzleCategories, string>();
+    private int _settings;
+    private const int SettingsNumber = 2;
+    public enum EPairNumber
+    {
+        NotSet = 0,
+        E10Pairs = 10,
+        E15Pairs = 15,
+        E20Pairs = 20,
+    }
+    public enum EPuzzleCategories
+    {
+        NotSet,
+        PuzzleOneFruits,
+        PuzzleTwoVegs
+    }
+    public struct Settings
+    {
+        public EPairNumber PairsNumber;
+        public EPuzzleCategories PuzzleCategory;
+    };
+
+    private Settings _gameSettings;
+
+    public static GameSettings Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            DontDestroyOnLoad(target:this);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(obj: this);
+        }
+    }
+    private void Start()
+    {
+        SetPuzzleGameDirectory();
+        _gameSettings = new Settings();
+        ResetGameSettings();
+    }
+
+    private void SetPuzzleGameDirectory()
+    {
+        _puzzleGameDirectory.Add(EPuzzleCategories.PuzzleOneFruits, "Fruits");
+        _puzzleGameDirectory.Add(EPuzzleCategories.PuzzleTwoVegs, "Vegs");
+    }
+
+    public void SetPairNumber(EPairNumber Number)
+    {
+        if(_gameSettings.PairsNumber == EPairNumber.NotSet)
+            _settings++;
+
+        _gameSettings.PairsNumber = Number;
+    }
+
+    public void SetPuzzleCategories(EPuzzleCategories cat)
+    {
+        if(_gameSettings.PuzzleCategory == EPuzzleCategories.NotSet)
+            _settings++;
+
+        _gameSettings.PuzzleCategory = cat;
+    }
+
+    public EPairNumber GetPairNumber()
+    {
+        return _gameSettings.PairsNumber;
+    }
+
+    public EPuzzleCategories GetPuzzleCategory()
+    {
+        return _gameSettings.PuzzleCategory;
+    }
+
+    public void ResetGameSettings()
+    {
+        _settings = 0;
+        _gameSettings.PuzzleCategory = EPuzzleCategories.NotSet;
+        _gameSettings.PairsNumber = EPairNumber.NotSet;
+    }
+
+    public bool AllSettingsReady()
+    {
+        return _settings == SettingsNumber;
+    }
+    public string GetMaterialDirectoryName()
+    {
+        return "Materials/";
+    }
+    public string GetPuzzleCategoryTextureDirectoryName()
+    {
+        if(_puzzleGameDirectory.ContainsKey(_gameSettings.PuzzleCategory))
+        {
+            return "Sprites/PuzzleGame/" + _puzzleGameDirectory[_gameSettings.PuzzleCategory] + "/";
+        }
+        else
+        {
+            Debug.LogError("ERROR: CANNOT GET DIRECTORY NAME");
+            return "";
+        }
+    }
+}
